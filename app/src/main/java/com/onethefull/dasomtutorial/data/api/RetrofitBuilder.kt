6d04dc9535fd1ac8e42f9e07ccfdb04601b7ccfb
sjw <jwseo2698@1thefull.com>
2026-01-13 14:11:09 +0900

    [sjw] feat: OkHttp SSL 인증서 검증 우회 로직 적용

--- app/src/main/java/com/onethefull/dasomtutorial/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/onethefull/dasomtutorial/data/api/RetrofitBuilder.kt
@@ -6,7 +6,13 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 /**
  * Created by sjw on 2021/11/10
@@ -19,17 +25,37 @@ object RetrofitBuilder {
     private fun getRetrofit(): Retrofit {
         val interceptor = HttpLoggingInterceptor(ApiLogger)
         interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val trustManager = getUnsafeTrustManager()
+        val sslSocketFactory = getNukeSSLSocketFactory(trustManager)
+
         val client = OkHttpClient.Builder()
             .connectTimeout(60, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(60, TimeUnit.SECONDS)
-            .addInterceptor(interceptor).build()
+            .addInterceptor(interceptor)
+            .sslSocketFactory(sslSocketFactory, trustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
 
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
+    }
+
+    private fun getUnsafeTrustManager(): X509TrustManager {
+        return object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        }
+    }
+
+    private fun getNukeSSLSocketFactory(trustManager: X509TrustManager): SSLSocketFactory {
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, arrayOf<TrustManager>(trustManager), SecureRandom())
+        return sslContext.socketFactory
     }
 
     val apiService: ApiService = getRetrofit().create(ApiService::class.java)
